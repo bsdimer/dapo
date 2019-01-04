cd22c44dff8d7f1f2b3be219bdd97dd50dddb33e
dimomass <dimitar.dimitrov@aossia.com>
2019-01-04 10:48:33 +0200

    add app module

---
+++ dev/dapo-website/src/main/ui/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+import { LoggerModule, NgxLoggerLevel } from "ngx-logger";
+import { environment } from "../environments/environment";
+import { CoreModule } from "./modules/core/core.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: '<GMAP-KEY>'
+    }),
+    LoggerModule.forRoot({
+      serverLoggingUrl: environment.loggerUri,
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.ERROR
+    }),
+    AgmSnazzyInfoWindowModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
