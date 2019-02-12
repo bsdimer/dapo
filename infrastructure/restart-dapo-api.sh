9dd48ca043a59d2db030701767ff6e3d7f7ed3f2
dimomass <dimitar.dimitrov@aossia.com>
2019-02-12 20:55:33 +0200

    Configure docker conteinerization

---
+++ infrastructure/restart-dapo-api.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+project=dapo-api
+tag=0.0.2-SNAPSHOT
+cid=`docker ps | grep $project | awk '{ print $1 }'`
+docker stop $cid
+docker container rm $cid
+docker image rm docker-registry.aossia.com:5000/$project:$tag
+docker image pull docker-registry.aossia.com:5000/dapo-api:$tag
+docker-compose create $project
+docker-compose start $project
